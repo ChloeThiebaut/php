d7d040cee00022280b3207a0e9142bc30f70b4de
ChloeThiebaut <chloe.thiebaut76@gmail.com>
2017-02-07 18:39:35 +0100

    Create footer.php

---
+++ site/footer.php
@@ -0,0 +1,8 @@
+    <footer>
+        <h3>test</h3>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/script.js"></script>
+    </footer>
+</body>
+</html>
